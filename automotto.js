a26d4eb213c4d1dc39b9bb49be6060066f917219
movai <vicente.queiroz@mov.ai>
2020-07-10 17:43:21 +0100

    Added login script

--- automotto.js
+++ automotto.js
@@ -1,10 +1,15 @@
-const { openBrowser, goto, write, click, repl } = require("taiko");
+const { openBrowser, goto, write, click, into, repl } = require("taiko");
+const { email, password } = require("./credentials");
+
 (async () => {
   try {
     await openBrowser();
+    console.log("Hello Taiko!!");
+    console.log(email, password);
     await goto("https://app.timemoto.com/");
-    await write("taiko test automation");
-    //await click("Google Search");
+    await write(email, into(textBox({ id: "form-login-email" })));
+    await write(password, into(textBox({ id: "form-login-password" })));
+    await click("Login");
 
     // Launchs the REPL after executing
     // the commands above
---
+++ credentials.js
@@ -0,0 +1,4 @@
+module.exports = Object.freeze({
+  email: "vicente.queiroz@mov.ai",
+  password: "R0b@t3233",
+});
