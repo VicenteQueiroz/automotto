db556bc9150c8b283f291e6d0afd80e19606aca0
movai <vicente.queiroz@mov.ai>
2020-07-10 16:49:42 +0100

    Added Taiko

---
+++ automotto.js
@@ -0,0 +1,18 @@
+const { openBrowser, goto, write, click, repl } = require("taiko");
+(async () => {
+  try {
+    await openBrowser();
+    await goto("google.com");
+    await write("taiko test automation");
+    await click("Google Search");
+
+    // Launchs the REPL after executing
+    // the commands above
+
+    await repl();
+  } catch (error) {
+    console.error(error);
+  } finally {
+    closeBrowser();
+  }
+})();
